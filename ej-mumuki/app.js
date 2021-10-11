0f8d20a760d21b5b7d681c756316ddbe37470885
Sandra Muraca <sandramuraca78@gmail.com>
2021-10-10 21:15:46 -0300

    ej 7 al 11

--- ej-mumuki/app.js
+++ ej-mumuki/app.js
@@ -4,6 +4,8 @@ Para comenzar este recorrido vamos a empezar creando nuestro primer objeto.
 
 Para ello vamos a declarar una variable que la vamos a llamar sobreMi, la misma debe definir un objeto con tres propiedades: nombre, apellido y edad.*/
 
+const { collapseTextChangeRangesAcrossMultipleVersions } = require("typescript")
+
 const  sobreMi = {
     nombre: "Sandra",
     apellido:"Muraca",
@@ -126,6 +128,126 @@ const modificarEdad = (datosUsuario) =>{
 }
 console.log(modificarEdad(datos))
 
+/*Ejercicio 7: Lenguaje favorito
+Tenemos un programa que almacena los datos de una persona.
+Necesitamos agregar la propiedad lenguajeFavorito si la propiedad programa es true, y asignarle como valor 'JavaScript'. Por ejemplo:
+agregarLenguajeFavorito(datos)
+datos = {
+  id: 1,
+  nombre: "Ada",
+  apellido: "Lovelace",
+  email: "ada.lovelace@gmail.com",
+  telefono: "1234567890",
+  edad: 29,
+  programa: true,
+  lenguajeFavorito: 'JavaScript'
+};*/
 
+
+const datos2 = {
+    id: 1,
+    nombre: "Ada",
+    apellido: "Lovelace",
+    email: "ada.lovelace@gmail.com",
+    telefono: "1234567890",
+    edad: 29,
+    programa: true,
+}
+
+const agregarLenguajeFavorito = (datosUsuario) =>{
+    if (datosUsuario.programa === true){
+        datosUsuario.lenguajeFavorito = "javascript"
+    }
+    return datosUsuario
+}
+
+console.log(agregarLenguajeFavorito(datos2))
+
+/*Ejercicio 8: Lenguaje favorito II
+Modificá la solución anterior, para que ahora el procedimiento se llame agregarLenguajesFavoritos y que agregue un array lenguajesFavoritos con los elementos 'JavaScript' y 'Ruby'.*/
+
+const agregarLenguasjeFavoritos = (datosUsuario) =>{
+    if (datosUsuario.programa === true){
+        datosUsuario.lenguajesFavoritos = ["javascript", "Rubi"]
+    }
+    return datosUsuario
+}
+
+console.log(agregarLenguasjeFavoritos(datos2))
+
+/*Ejercicio 9: Disco
+Queremos obtener el nombre, año de lanzamiento y banda de un disco , para mostrar el mensaje "El disco [NOMBRE DISCO] de la banda [NOMBRE DE LA BANDA] se lanzó en el año [AÑO DE LANZAMIENTO DEL DISCO]"*/
+let disco = {
+    id: 1,
+    nombre: 'Wasting Light',
+    anioLanzamiento: 2011,
+    cantidadDeTemas: 12,
+    banda: {
+      nombre: 'Foo Fighters',
+      anioFormacion: 1994
+    }
+  }
+
+  const infoDelDisco = (infoDisco) => {
+    return `El disco ${infoDisco.nombre} de la banda ${infoDisco.banda.nombre} de lanzo en el año ${infoDisco.anioLanzamiento}`
+  }
+
+  console.log(infoDelDisco(disco))
+
+  /*Ejercicio 10: Habilidades
+Contamos con un programa que almacena perfiles de LinkedIn con el siguiente formato:
+let perfil = {
+  id: 123456789,
+  nombre: "Ada Lovelace",
+  url: "https://www.linkedin.com/in/ada-lovelace",
+  habilidades: ["HTML", "CSS", "SASS"],
+}
+queremos agregarle un elemento en la propiedad habilidades*/
+
+let perfil3 = {
+    id: 123456789,
+    nombre: "Ada Lovelace",
+    url: "https://www.linkedin.com/in/ada-lovelace",
+    habilidades: ["HTML", "CSS", "SASS"]
+}
+
+let nuevaHabilidad = "javascript"
+
+const agregarHabilidad =(datosUser,habilidad) => {
+    return datosUser.habilidades.push(habilidad)
+}
+
+agregarHabilidad(perfil3, nuevaHabilidad)
+console.log(perfil3)
+
+/*Ejercicio 11: Playlist
+Necesitamos obtener la información de las playlist de una conocida plataforma para escuchar música online . Las playlist son objetos con el siguiente formato...*/
+
+let playlistDeEjemplo = {
+    nombre: "Lista de Nirvana",
+    privada: true,
+    canciones: ["Smells Like Teen Spirit", "In Bloom", "Come As You Are"]
+}
+
+const infoDeLaPlaylist = (playlist) =>{
+   
+    let estadoPlaylist= ""
+    
+    if(playlist.privada === true){
+        estadoPlaylist = "si"
+    } else{
+        estadoPlaylist = "no"
+    }
+    
+    return `
+            ${playlist.nombre}
+            Privada: ${estadoPlaylist}
+            Canciones:
+            ${playlist.canciones[0]}
+            ${playlist.canciones[1]}
+            ${playlist.canciones[2]}`
+}
+
+console.log(infoDeLaPlaylist(playlistDeEjemplo))
 
 
